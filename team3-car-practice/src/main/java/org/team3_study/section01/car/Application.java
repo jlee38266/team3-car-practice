e1c2061faec947e5a99e532b04e7fd4d18ea57e1
CJC0512 <ehdchs2006@gmail.com>
2024-01-17 17:11:25 +0900

    24.01.17 markdown test
    24.01.17 markdown 확인 차 2트 commit

--- team3-car-practice/src/main/java/org/team3_study/section01/car/Application.java
+++ team3-car-practice/src/main/java/org/team3_study/section01/car/Application.java
@@ -3,5 +3,6 @@ package org.team3_study.section01.car;
 public class Application {
     public static void main(String[] args) {
         System.out.println("마크다운 확인차.");
+        System.out.println("markdown check");
     }
 }
