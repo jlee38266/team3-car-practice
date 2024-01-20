1eb29e62bac2e09ef804c1c0c37a754f44dd52a6
CJC0512 <ehdchs2006@gmail.com>
2024-01-20 13:53:16 +0900

    CJC0512 class 작성
    introducemyself, planning print문 작성

---
+++ team3-car-practice/src/main/java/org/team3_study/section01/car/CJC0512.java
@@ -0,0 +1,14 @@
+package org.team3_study.section01.car;
+
+public class CJC0512 extends TeamMember{
+
+    @Override
+    public void introduceMySelf() {
+        System.out.println("안녕하세요! 김포사는 청년 최종찬입니다~!!");
+    }
+
+    @Override
+    public void planning() {
+        System.out.println("제 계획은 돈도 많고 시간도 많은 프리랜서입니다!!");
+    }
+}
