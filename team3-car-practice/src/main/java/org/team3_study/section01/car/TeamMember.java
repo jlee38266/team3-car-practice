38f22f15a8241ae54b9de387471912c5acab4627
jhlee <jun73045928@gmail.com>
2024-01-20 13:46:30 +0900

    추상 클래스 복습
    TeamMember 추상 클래스와 TeamMember의 추상 메소드 정의

---
+++ team3-car-practice/src/main/java/org/team3_study/section01/car/TeamMember.java
@@ -0,0 +1,9 @@
+package org.team3_study.section01.car;
+
+public abstract class TeamMember {
+
+    /* 설명. 해당 메소드 재정의 시 결과는 모두 println으로 출력! */
+    public abstract void introduceMySelf();
+    public abstract void planning();
+
+}
